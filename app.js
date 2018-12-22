aba16c3a69c81c19a30679d317ca213bdc7f4c07
Marcus Pearce <marcuspearce7@gmail.com>
2018-12-22 04:51:28 +0000

    styled homepage, added navbar, added custom stylesheets w/ type-out-effect code

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 var express = require("express");
 var app = express();
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); //expect ejs in rendering
+app.use(express.static(__dirname + "/public")); //to access public folder
 
 
 // ********** ROUTES **********
